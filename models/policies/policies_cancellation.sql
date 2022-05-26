6bbaf93eb0fa034f497a8d84313345b2ac1b5c00
Trong Le <trong@covertree.com>
2022-05-26 12:38:11 -0500

    adding fivetran_deleted clause

--- models/policies/policies_cancellation.sql
+++ models/policies/policies_cancellation.sql
@@ -10,4 +10,5 @@
 {{ config(materialized='table') }}
 
 select pk, sk, cancellation_category, effective_timestamp, documents, created_timestamp, title, policy_modification_locator, price, conflict_handling, name, issued_timestamp,
-policy_locator, state, locator, cancellation_comments, invoice_locator, reinstatement from dynamodb_develop.deve_socotra_policy_table where pk like 'POLICY#%' and sk like 'CANCELLATION%'
+policy_locator, state, locator, cancellation_comments, invoice_locator, reinstatement from dynamodb_develop.deve_socotra_policy_table
+where (pk like 'POLICY#%' and sk like 'CANCELLATION%') and _fivetran_deleted='FALSE'
