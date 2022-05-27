ad1784befb860b1c8d76ce37e7b3e588588ec0c0
Trong Le <trong@covertree.com>
2022-05-27 09:34:29 -0500

    added future_invoice, removed comments

--- models/policies/policies_cancellation.sql
+++ models/policies/policies_cancellation.sql
@@ -1,12 +1,3 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
 select pk, sk, cancellation_category, effective_timestamp, documents, created_timestamp, title, policy_modification_locator, price, conflict_handling, name, issued_timestamp,
--- models/policies/policies_default_coverages.sql
+++ models/policies/policies_default_coverages.sql
@@ -1,12 +1,3 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
 select pk, sk, pricing from dynamodb_develop.deve_socotra_policy_table
