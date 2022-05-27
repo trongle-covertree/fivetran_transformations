3937ddc2afb3da59be578455eb8a48515677eeb3
Trong Le <trong@covertree.com>
2022-05-27 09:47:00 -0500

    fixed sk for future_invoice query

--- models/policies/policies_future_invoice.sql
+++ models/policies/policies_future_invoice.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
 select pk, sk, write_off, financial_transactions, start_timestamp, end_timestamp, issue_timestamp, due_timestamp from dynamodb_develop.deve_socotra_policy_table
-where (pk like 'POLICY#%' and sk like 'FUTURE_INVOICE#%') and _fivetran_deleted='FALSE'
+where (pk like 'POLICY#%' and sk like 'FUTUREINVOICE#%') and _fivetran_deleted='FALSE'
