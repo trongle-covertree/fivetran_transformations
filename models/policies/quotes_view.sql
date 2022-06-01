7d4fd1085d78ca2a8ac98ac6372d0567ccb23ec0
Trong Le <trong@covertree.com>
2022-06-01 10:04:18 -0500

    trying out snowflake functions

--- models/policies/quotes_view.sql
+++ models/policies/quotes_view.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='view') }}
 
 select 
-  sum(case when EXPOSURES LIKE '%Silver%' then 1 end) as Silver,
-  sum(case when EXPOSURES LIKE '%Gold%' then 1 end) as Gold,
-  sum(case when EXPOSURES LIKE '%Platinum%' then 1 end) as Platinum
+  count_if(EXPOSURES LIKE '%Silver%') as silver,
+  count_if(EXPOSURES LIKE '%Gold%') as gold,
+  count_if(EXPOSURES LIKE '%Platinum%') as platinum
 from policies_policy
 where pk like 'POLICY#%' and sk = 'POLICY' and (status = 'Pending-Cancellation' or status = 'Policy-Activated' or status = 'Pending-Esign Required')
