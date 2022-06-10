ed38f26edd3604efd969dbd4f0b4e6c1c3258005
Trong Le <trong@covertree.com>
2022-06-10 10:01:57 -0500

    removed datediff for sync, rearranged table

--- models/develop/policies/develop_policies_quote_plans.sql
+++ models/develop/policies/develop_policies_quote_plans.sql
@@ -3,16 +3,16 @@
 select
   pk,
   sk,
-  silver_quote_pricing,
-  customization,
-  platinum_quote_locator,
   silver_quote_locator,
-  descriptions,
-  gold_quote_data,
-  platinum_quote_pricing,
+  silver_quote_data,
+  silver_quote_pricing,
   gold_quote_locator,
+  gold_quote_data,
   gold_quote_pricing,
+  platinum_quote_locator,
   platinum_quote_data,
-  silver_quote_data
+  platinum_quote_pricing,
+  customization,
+  descriptions
 from dynamodb_develop.deve_socotra_policy_table
 where (pk like 'POLICY#%' and sk like 'QUOTE_PLANS') and _fivetran_deleted='FALSE'
