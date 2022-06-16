b9acf2f8d818561651012289f2cb37f0a33b6a68
Trong Le <trong@covertree.com>
2022-06-16 08:25:44 -0500

    added territory_factor table

---
+++ models/develop/territory_factor/develop_territory_factor.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='table') }}
+
+select
+  PK,
+  grid_id,
+  primary_census_tract,
+  primary_class,
+  primary_county_fips,
+  primary_county_name,
+  primary_island,
+  primary_place_name,
+  primary_po_name,
+  primary_ua_label,
+  primary_ua_type,
+  primary_zillow_community_name,
+  primary_zip_code,
+  state_fips,
+  state_name,
+  sub_region,
+  territory_aop,
+  territory_eq,
+  territory_flood,
+  territory_hurr,
+  territory_wh,
+  territory_wildfire,
+  uw_admitted
+from dynamodb_develop.deve_territory_factor_table
+where _fivetran_deleted='FALSE'
