c643be6c7afb683243f5a7531141301d34139554
Trong Le <trong@covertree.com>
2022-08-04 14:55:50 -0500

    trying out casting

--- models/develop/policies/develop_policies_policy.sql
+++ models/develop/policies/develop_policies_policy.sql
@@ -1,5 +1,18 @@
 {{ config(materialized='table') }}
 
+with casted_timestamps as (
+  select
+    created_timestamp::string,
+    issued_timestamp::string,
+    original_contract_start_timestamp::string,
+    updated_timestamp::string,
+    effective_contract_end_timestamp::string,
+    original_contract_end_timestamp::string
+
+    from dynamodb_develop.deve_socotra_policy_table
+    where (pk like 'POLICY#%' and sk like 'POLICY') and _fivetran_deleted='FALSE'
+)
+
 select
   {{ dbt_utils.surrogate_key(
     'pk',
@@ -8,23 +21,23 @@ select
   pk,
   sk,
   documents,
-  {{ dbt_date.from_unixtimestamp("created_timestamp") }} as created_timestamp,
-  {{ dbt_date.from_unixtimestamp("issued_timestamp") }} as issued_timestamp,
+  {{ dbt_date.from_unixtimestamp("casted_timestamps.created_timestamp") }} as created_timestamp,
+  {{ dbt_date.from_unixtimestamp("casted_timestamps.issued_timestamp") }} as issued_timestamp,
   fees,
   characteristics,
   config_version,
   gross_fees,
   payment_schedule_name,
-  {{ dbt_date.from_unixtimestamp("original_contract_start_timestamp") }} as original_contract_start_timestamp,
-  {{ dbt_date.from_unixtimestamp("updated_timestamp") }} as updated_timestamp,
+  {{ dbt_date.from_unixtimestamp("casted_timestamps.original_contract_start_timestamp") }} as original_contract_start_timestamp,
+  {{ dbt_date.from_unixtimestamp("casted_timestamps.updated_timestamp") }} as updated_timestamp,
   product_name,
   exposures,
   invoices,
-  {{ dbt_date.from_unixtimestamp("effective_contract_end_timestamp") }} as effective_contract_end_timestamp,
+  {{ dbt_date.from_unixtimestamp("casted_timestamps.effective_contract_end_timestamp") }} as effective_contract_end_timestamp,
   product_locator,
   policyholder_locator,
   gross_fees_currency,
-  {{ dbt_date.from_unixtimestamp("original_contract_end_timestamp") }} as original_contract_end_timestamp,
+  {{ dbt_date.from_unixtimestamp("casted_timestamps.original_contract_end_timestamp") }} as original_contract_end_timestamp,
   display_id,
   modifications,
   locator,
