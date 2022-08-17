93dcc58ccdaa9307c19cedf4552603a177faaa24
Trong Le <trong@covertree.com>
2022-08-17 09:43:54 -0500

    only changing locators

--- models/prod/quotes/prod_quotes.sql
+++ models/prod/quotes/prod_quotes.sql
@@ -3,12 +3,12 @@
 select
   PK,
   SK,
-  {{ dbt_date.from_unixtimestamp("created_at::string", format="milliseconds") }} as CREATED_AT,
+  CREATED_AT,
   DESCRIPTION,
   POLICY_LOCATOR::string,
   QUOTE_LOCATOR::string,
   STATUS,
-  {{ dbt_date.from_unixtimestamp("updated_at::string", format="milliseconds") }} as UPDATED_AT,
+  UPDATED_AT,
   CONVERTED
 from dynamodb.prod_socotra_quote_table
 where (pk like 'QUOTE#%' and sk like 'POLICY#%') and _fivetran_deleted='FALSE'
