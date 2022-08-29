8db7a8c06c02d2101bfbfc42ac9cbed6d9fb4763
Trong Le <trong@covertree.com>
2022-08-29 17:07:06 -0500

    reverted develop quotes model

--- models/develop/quotes/develop_quotes.sql
+++ models/develop/quotes/develop_quotes.sql
@@ -3,12 +3,12 @@
 select
   PK,
   SK,
-  {{ dbt_date.from_unixtimestamp("created_at", format="milliseconds") }} as CREATED_AT,
+  CREATED_AT,
   DESCRIPTION,
   POLICY_LOCATOR,
   QUOTE_LOCATOR,
   STATUS,
-  {{ dbt_date.from_unixtimestamp("updated_at", format="milliseconds") }} as UPDATED_AT,
+  UPDATED_AT,
   CONVERTED
 from dynamodb_develop.deve_socotra_quote_table
 where (pk like 'QUOTE#%' and sk like 'POLICY#%') and _fivetran_deleted='FALSE'
