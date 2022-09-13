8bae262452c2b1dccd889ab7129de59ef438af46
Trong Le <trong@covertree.com>
2022-09-13 12:04:20 -0400

    commenting out prod characteristics

--- models/prod/misc/prod_policy_characteristics.sql
+++ models/prod/misc/prod_policy_characteristics.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='table') }}
+-- {{ config(materialized='table') }}
 
-{{ run_characteristics( env='transformations_dynamodb', prefix='prod' )}}
+-- {{ run_characteristics( env='transformations_dynamodb', prefix='prod' )}}
