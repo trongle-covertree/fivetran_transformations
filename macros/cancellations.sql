1ab2fdc99e9384576240bea3dc94011da0dc71ba
Trong Le <trong@covertree.com>
2022-10-25 08:31:07 -0500

    trying to_double and stringing value

--- macros/cancellations.sql
+++ macros/cancellations.sql
@@ -24,7 +24,7 @@ from {{ env }}.{{ prefix }}_policies_cancellation
 {% if prices|length > 0 %}
     SELECT  Column1 AS ID, Column2 AS PK, Column3 AS SK, Column4 AS POLICY_MODIFICATION_LOCATOR, parse_json(Column5) AS COMMISSIONS, parse_json(Column6) AS EXPOSURE_PRICES,
         parse_json(Column7) AS FEES, Column8 AS GROSS_COMMISSIONS_CHANGE, Column9 AS GROSS_FEES_CHANGE, Column10 AS GROSS_PREMIUM_CHANGE, Column11 AS GROSS_TAXES_CHANGE,
-        parse_json(Column12) AS HOLDBACKS, to_number(Column13, 10, 2) AS NEW_GROSS_COMMISSIONS, Column14 AS NEW_GROSS_FEES, Column15 AS NEW_GROSS_PREMIUM,
+        parse_json(Column12) AS HOLDBACKS, to_double(Column13) AS NEW_GROSS_COMMISSIONS, Column14 AS NEW_GROSS_FEES, Column15 AS NEW_GROSS_PREMIUM,
         Column16 AS NEW_GROSS_TAXES, Column17 AS NEW_TOTAL, parse_json(Column18) AS TAX_GROUPS, Column19 AS TOTAL_CHANGE, to_timestamp(Column20) as CREATED_TIMESTAMP,
         to_timestamp(Column21) AS ISSUED_TIMESTAMP, to_timestamp(Column22) as EFFECTIVE_TIMESTAMP
     FROM VALUES 
@@ -39,7 +39,7 @@ from {{ env }}.{{ prefix }}_policies_cancellation
         '{{ tojson(price_json.commissions) }}',
         '{{ tojson(price_json.exposurePrices) | replace("'", "\\'") }}',
         '{{ tojson(price_json.fees) }}',
-        {{ price_json.grossCommissionsChange or 'null' }},
+        '{{ price_json.grossCommissionsChange or null }}',
         {{ price_json.grossFeesChange or 'null' }},
         {{ price_json.grossPremiumChange or 'null' }},
         {{ price_json.grossTaxesChange or 'null' }},
