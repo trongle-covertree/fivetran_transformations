5ce6231bf8e9c4cd7ec5af66729c24cd0c41c3f9
Trong Le <trong@covertree.com>
2022-10-26 15:10:58 -0500

    trying unique key

--- models/develop/misc/develop_policy_exposures_address.sql
+++ models/develop/misc/develop_policy_exposures_address.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental', unique_key='id') }}
 
 {{ run_exposures_address( env='transformations_dynamodb_develop', prefix='develop' )}}
