be6e9799cdf47cefb544d3ba43ab2eafe027583a
Trong Le <trong@covertree.com>
2022-11-02 08:51:18 -0500

    trying without perils

--- models/develop_exposure_perils/develop_policy_exposures_perils.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized='incremental',
-    post_hook = "DELETE FROM FIVETRAN_COVERTREE.TRANSFORMATIONS_DYNAMODB_DEVELOP.DEVELOP_POLICY_EXPOSURES_PERILS where PK = 'NO FIELDS'") }}
-
-{{ run_exposures_perils( env='transformations_dynamodb_develop', prefix='develop' )}}
