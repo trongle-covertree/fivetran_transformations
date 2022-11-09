d7e375f1f82e9fa2664284ae25084e6271a8998e
Trong Le <trong@covertree.com>
2022-11-09 09:18:15 -0600

    added unique row view for characteristics

---
+++ models/develop/misc/develop_policy_characteristics_unique_view.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }}
+
+select *
+FROM
+(
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY pk ORDER BY created_timestamp desc, start_timestamp desc) num
+    FROM {{ ref('develop_policy_characteristics')}}
+) inn
+where inn.num = 1;
