9ab0ac66ac406470fce15443f967553a483b0234
Trong Le <trong@covertree.com>
2022-11-09 09:54:16 -0600

    removed semicolon for unique view

--- models/develop/misc/develop_policy_characteristics_unique_view.sql
+++ models/develop/misc/develop_policy_characteristics_unique_view.sql
@@ -7,4 +7,4 @@ FROM
         ROW_NUMBER() OVER (PARTITION BY pk ORDER BY created_timestamp desc, start_timestamp desc) num
     FROM {{ ref('develop_policy_characteristics')}}
 ) inn
-where inn.num = 1;
+where inn.num = 1
