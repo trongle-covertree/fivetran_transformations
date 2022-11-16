8da5d98d4ae23860587689dec5f93e61adb655d7
Trong Le <trong@covertree.com>
2022-11-16 16:02:06 -0600

    converting home_info unique values

--- models/prod/misc/prod_policy_home_info_unique_view.sql
+++ models/prod/misc/prod_policy_home_info_unique_view.sql
@@ -3,8 +3,10 @@
 select *
 FROM
 (
-    SELECT *,
-        ROW_NUMBER() OVER (PARTITION BY pk ORDER BY created_timestamp desc) num
+    SELECT id, pk, to_decimal(roof_year_yyyy) as roof_year_yyyy, skirting_type, roof_shape, roof_material, home_hud_number,
+        to_decimal(total_square_footage) as total_square_footage, home_type, to_decimal(model_year) as model_year, manufacturer_name,
+        roof_condition, home_fixtures, inspection_interior, inspection_exterior, inspection_aerial, created_timestamp, updated_timestamp,
+        policy_created_timestamp, policy_updated_timestamp,
     FROM {{ ref('prod_policy_exposures_home_info') }}
 ) inn
 where inn.num = 1
