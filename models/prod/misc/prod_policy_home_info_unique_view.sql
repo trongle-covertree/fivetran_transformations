297c3bfae95231ae209832d5ee393c9a64518011
Trong Le <trong@covertree.com>
2022-11-16 16:10:52 -0600

    accidentally removed ordering with partition

--- models/prod/misc/prod_policy_home_info_unique_view.sql
+++ models/prod/misc/prod_policy_home_info_unique_view.sql
@@ -7,6 +7,7 @@ FROM
         to_decimal(total_square_footage) as total_square_footage, home_type, to_decimal(model_year) as model_year, manufacturer_name,
         roof_condition, home_fixtures, inspection_interior, inspection_exterior, inspection_aerial, created_timestamp, updated_timestamp,
         policy_created_timestamp, policy_updated_timestamp,
+        ROW_NUMBER() OVER (PARTITION BY pk ORDER BY created_timestamp desc) num
     FROM {{ ref('prod_policy_exposures_home_info') }}
 ) inn
 where inn.num = 1
