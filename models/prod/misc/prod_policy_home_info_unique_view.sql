fa98fc8af452d231bc5696f2f6c899c68788f777
Trong Le <trong@covertree.com>
2022-11-16 15:15:17 -0600

    forgot to switch from table for unique home info

--- models/prod/misc/prod_policy_home_info_unique_view.sql
+++ models/prod/misc/prod_policy_home_info_unique_view.sql
@@ -5,6 +5,6 @@ FROM
 (
     SELECT *,
         ROW_NUMBER() OVER (PARTITION BY pk ORDER BY created_timestamp desc) num
-    FROM {{ ref('prod_policy_exposures_plan_information') }}
+    FROM {{ ref('prod_policy_exposures_home_info') }}
 ) inn
 where inn.num = 1
