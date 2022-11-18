d4cc3f834953a74db62f37c5fe9a9a959ca2688c
Trong Le <trong@covertree.com>
2022-11-18 17:01:38 -0600

    added post hook so ct_mhcids dont get wiped

--- models/prod/misc/prod_policy_exposures_grid_info.sql
+++ models/prod/misc/prod_policy_exposures_grid_info.sql
@@ -1,3 +1,5 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental',
+    post_hook = "update FIVETRAN_COVERTREE.TRANSFORMATIONS_DYNAMODB.prod_policy_exposures_grid_info g set ct_mhcid = h.ct_mhcid from FIVETRAN_COVERTREE.TRANSFORMATIONS_DYNAMODB.prod_mhcid_historical h where g.pk = h.pk")
+}}
 
 {{ run_exposures_grid_info( env='transformations_dynamodb', prefix='prod' )}}
