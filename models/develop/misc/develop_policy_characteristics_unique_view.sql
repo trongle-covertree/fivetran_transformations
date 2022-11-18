b9fa3e826858962b0976a19815df4e58d885e440
Trong Le <trong@covertree.com>
2022-11-18 10:20:59 -0600

    removed trailing comma

--- models/develop/misc/develop_policy_characteristics_unique_view.sql
+++ models/develop/misc/develop_policy_characteristics_unique_view.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
 
-SELECT *,
+SELECT *
 FROM {{ ref('develop_policy_characteristics')}}
 where (start_timestamp < current_timestamp() and end_timestamp > current_timestamp() and replaced_timestamp is null) or (start_timestamp = end_timestamp)
--- models/prod/misc/prod_policy_characteristics_unique_view.sql
+++ models/prod/misc/prod_policy_characteristics_unique_view.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 
-SELECT *,
+SELECT *
 FROM {{ ref('prod_policy_characteristics')}}
 where (start_timestamp < current_timestamp() and end_timestamp > current_timestamp() and replaced_timestamp is null) or (start_timestamp = end_timestamp)
