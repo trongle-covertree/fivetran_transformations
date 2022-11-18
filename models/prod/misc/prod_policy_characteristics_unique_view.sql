ba2251b5c7ae1cd0fdcb1bac3c888f7b26d64f31
Trong Le <trong@covertree.com>
2022-11-18 14:11:24 -0600

    added characteristics for future issued policies

--- models/prod/misc/prod_policy_characteristics_unique_view.sql
+++ models/prod/misc/prod_policy_characteristics_unique_view.sql
@@ -2,4 +2,4 @@
 
 SELECT *
 FROM {{ ref('prod_policy_characteristics')}}
-where (start_timestamp < current_timestamp() and end_timestamp > current_timestamp() and replaced_timestamp is null) or (start_timestamp = end_timestamp)
+where (start_timestamp < current_timestamp() and end_timestamp > current_timestamp() and replaced_timestamp is null) or (start_timestamp = end_timestamp) or (current_timestamp() < start_timestamp and replaced_timestamp is null)
