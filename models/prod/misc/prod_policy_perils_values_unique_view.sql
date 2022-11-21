03ce2b30fb29b18f7257a3511b8f631871439b55
Trong Le <trong@covertree.com>
2022-11-21 16:17:25 -0600

    forgot database and schema

--- models/prod/misc/prod_policy_perils_values_unique_view.sql
+++ models/prod/misc/prod_policy_perils_values_unique_view.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
 select pk, peril_locator, coverage, name, value, spp_desc, spp_type, spp_value, max(created_timestamp) as created_timestamp
-from prod_policy_exposures_perils_values
+from fivetran_covertree.transformations_dynamodb.prod_policy_exposures_perils_values
 group by pk, peril_locator, coverage, name, value, spp_desc, spp_type, spp_value
 order by created_timestamp desc
