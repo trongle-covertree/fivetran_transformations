3805de1015a1b94543c3b6f49d25ba8d31a98278
Trong Le <trong@covertree.com>
2022-11-21 16:10:42 -0600

    removed trailing ;

--- models/prod/misc/prod_policy_perils_values_unique_view.sql
+++ models/prod/misc/prod_policy_perils_values_unique_view.sql
@@ -3,4 +3,4 @@
 select pk, peril_locator, coverage, name, value, spp_desc, spp_type, spp_value, max(created_timestamp) as created_timestamp
 from prod_policy_exposures_perils_values
 group by pk, peril_locator, coverage, name, value, spp_desc, spp_type, spp_value
-order by created_timestamp desc;
+order by created_timestamp desc
