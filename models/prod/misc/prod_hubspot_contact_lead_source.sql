8c024bc18e9c0d454d0252cf8100b5b0e44db364
Trong Le <trong@covertree.com>
2022-12-06 15:08:50 -0600

    forgot env and db

--- models/prod/misc/prod_hubspot_contact_lead_source.sql
+++ models/prod/misc/prod_hubspot_contact_lead_source.sql
@@ -2,4 +2,4 @@
 
 select id, property_lead_source as lead_source, property_email as customer_email, parse_url(property_hs_analytics_last_referrer):host::varchar as source_hostname,
     parse_url(property_hs_analytics_last_url):host::varchar as referred_hostname, property_usa_state as state
-from contact where property_email != 'test-dont-pay-me-for-this@gmail.com';
+from fivetran_covertree.hubspot.contact where property_email != 'test-dont-pay-me-for-this@gmail.com';
