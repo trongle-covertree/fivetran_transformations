76d5ad9f82b58dcf349f0ca670f92130180a3e94
Trong Le <trong@covertree.com>
2022-12-06 14:56:58 -0600

    added contact lead source table

---
+++ models/prod/misc/prod_hubspot_contact_lead_source.sql
@@ -0,0 +1,5 @@
+{{ config(materialized='table') }}
+
+select id, property_lead_source as lead_source, property_email as customer_email, parse_url(property_hs_analytics_last_referrer):host::varchar as source_hostname,
+    parse_url(property_hs_analytics_last_url):host::varchar as referred_hostname, property_usa_state as state
+from contact where property_email != 'test-dont-pay-me-for-this@gmail.com';
