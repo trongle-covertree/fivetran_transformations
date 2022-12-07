ea46b0176841db67008073d9a877b29debd813ed
Trong Le <trong@covertree.com>
2022-12-07 09:50:58 -0600

    added post_hook to add in park_name from community lookup table

--- models/prod/misc/prod_policy_plan_information_unique_view.sql
+++ models/prod/misc/prod_policy_plan_information_unique_view.sql
@@ -1,4 +1,5 @@
-{{ config(materialized='table') }}
+{{ config(materialized='table',
+    post_hook = "UPDATE FIVETRAN_COVERTREE.TRANSFORMATIONS_DYNAMODB.prod_policy_plan_information_unique_view t1 SET park_name = t2.name FROM (select g.pk as pk, cpl.name as name from FIVETRAN_COVERTREE.TRANSFORMATIONS_DYNAMODB.communities_partner_lookup as cpl inner join FIVETRAN_COVERTREE.TRANSFORMATIONS_DYNAMODB.prod_policy_exposures_grid_info as g on g.ct_mhcid = cpl.ct_mhcid) t2 WHERE park_name is null and t1.pk = t2.pk") }}
 
 select *
 FROM
