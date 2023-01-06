af95dd83a3b339f28e333d6fa40605abaf021d64
Trong Le <trong@covertree.com>
2023-01-06 11:09:40 -0600

    added env and db

--- models/prod/misc/prod_policies_quotes_live_status.sql
+++ models/prod/misc/prod_policies_quotes_live_status.sql
@@ -4,8 +4,8 @@ SELECT sk, status, created_timestamp, updated_timestamp
 FROM
   ( select distinct sk, status, to_timestamp(to_varchar(created_at)) as created_timestamp, to_timestamp(to_varchar(updated_at)) as updated_timestamp
          , ROW_NUMBER() OVER(partition by sk order by SK, case when status = 'Quote-Expired' or status = 'Initial-Quote' then 1 else 0 end) AS RowNumber
-    FROM prod_quotes t
+    FROM fivetran_covertree.transformations_dynamodb.prod_quotes t
   ) AS tmp
 WHERE RowNumber = 1 
 UNION
-SELECT PK, STATUS, created_timestamp, updated_timestamp from prod_policies_policy where status != 'Initial-Quote'
+SELECT PK, STATUS, created_timestamp, updated_timestamp from fivetran_covertree.transformations_dynamodb.prod_policies_policy where status != 'Initial-Quote'
