ca8bf02c267b5744926708446a2cf72cc3e748bb
Trong Le <trong@covertree.com>
2023-01-06 10:54:50 -0600

    added statuses for quotes/policies

---
+++ models/prod/misc/prod_policies_quotes_live_status.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+SELECT sk, status, created_timestamp, updated_timestamp
+FROM
+  ( select distinct sk, status, to_timestamp(to_varchar(created_at)) as created_timestamp, to_timestamp(to_varchar(updated_at)) as updated_timestamp
+         , ROW_NUMBER() OVER(partition by sk order by SK, case when status = 'Quote-Expired' or status = 'Initial-Quote' then 1 else 0 end) AS RowNumber
+    FROM prod_quotes t
+  ) AS tmp
+WHERE RowNumber = 1 
+UNION
+SELECT PK, STATUS, created_timestamp, updated_timestamp from prod_policies_policy where status != 'Initial-Quote'
