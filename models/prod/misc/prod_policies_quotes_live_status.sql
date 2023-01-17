44f4f56a66f565cc44ffa39d0a3caf446e216e55
Trong Le <trong@covertree.com>
2023-01-17 14:06:26 -0600

    using quote_locator to connect quote statuses

--- models/prod/misc/prod_policies_quotes_live_status.sql
+++ models/prod/misc/prod_policies_quotes_live_status.sql
@@ -1,11 +1,8 @@
 {{ config(materialized='table') }}
 
-SELECT sk AS PK, status, created_timestamp, updated_timestamp
+SELECT PK, status, created_timestamp, updated_timestamp
 FROM
-  ( select distinct sk, status, to_timestamp(to_varchar(created_at)) as created_timestamp, to_timestamp(to_varchar(updated_at)) as updated_timestamp
-         , ROW_NUMBER() OVER(partition by sk order by SK, case when status = 'Quote-Expired' or status = 'Initial-Quote' then 1 else 0 end) AS RowNumber
-    FROM fivetran_covertree.transformations_dynamodb.prod_quotes t
+  ( select p.pk AS PK, q.status as STATUS, created_timestamp, updated_timestamp from prod_policies_policy as p inner join fivetran_covertree.transformations_dynamodb.prod_quotes as q on p.quote_locator = q.quote_locator where p.status = 'Initial-Quote'
   ) AS tmp
-WHERE RowNumber = 1 
 UNION
 SELECT PK, STATUS, created_timestamp, updated_timestamp from fivetran_covertree.transformations_dynamodb.prod_policies_policy where status != 'Initial-Quote'
