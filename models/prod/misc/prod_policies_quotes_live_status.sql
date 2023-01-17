5f26c1bf6bf53a2729d33c5f0756e3ddbb0d49f9
Trong Le <trong@covertree.com>
2023-01-17 15:53:59 -0600

    forgot env for quote statuses

--- models/prod/misc/prod_policies_quotes_live_status.sql
+++ models/prod/misc/prod_policies_quotes_live_status.sql
@@ -2,7 +2,7 @@
 
 SELECT PK, status, created_timestamp, updated_timestamp
 FROM
-  ( select p.pk AS PK, q.status as STATUS, created_timestamp, updated_timestamp from prod_policies_policy as p inner join fivetran_covertree.transformations_dynamodb.prod_quotes as q on p.quote_locator = q.quote_locator where p.status = 'Initial-Quote'
+  ( select p.pk AS PK, q.status as STATUS, created_timestamp, updated_timestamp from fivetran_covertree.transformations_dynamodb.prod_policies_policy as p inner join fivetran_covertree.transformations_dynamodb.prod_quotes as q on p.quote_locator = q.quote_locator where p.status = 'Initial-Quote'
   ) AS tmp
 UNION
 SELECT PK, STATUS, created_timestamp, updated_timestamp from fivetran_covertree.transformations_dynamodb.prod_policies_policy where status != 'Initial-Quote'
