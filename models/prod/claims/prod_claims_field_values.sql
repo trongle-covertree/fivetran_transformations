3c48d20b9cc052a783b3d21bbb88c8d951a8bdb7
Trong Le <trong@covertree.com>
2023-01-17 16:08:25 -0600

    added claim_locator and only pulling policy# pks

--- models/prod/claims/prod_claims_field_values.sql
+++ models/prod/claims/prod_claims_field_values.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
-select field_values:documents as documents, to_varchar(field_values:injured_party_info[0]) as injured_party_info,
+select sk AS claim_locator, field_values:documents as documents, to_varchar(field_values:injured_party_info[0]) as injured_party_info,
     to_varchar(field_values:loss_address[0]) as loss_address, to_varchar(field_values:loss_type[0]) as loss_type, to_varchar(field_values:policy_type[0]) as policy_type,
     to_varchar(field_values:property_city[0]) as property_city, to_varchar(field_values:property_country[0]) as property_country,
     to_varchar(field_values:property_county[0]) as property_county, to_varchar(field_values:property_zip_code[0]) as property_zip_code,
@@ -13,4 +13,4 @@ select field_values:documents as documents, to_varchar(field_values:injured_part
     to_varchar(field_values:source_of_damage_water_damage[0]) as source_of_damage_water_damage, to_varchar(field_values:water_damage_loss_description[0]) as water_damage_loss_description,
     to_varchar(field_values:water_damage_repaired[0]) as water_damage_repaired, to_varchar(field_values:liability_claim_reason[0]) as liability_claim_reason,
     to_varchar(field_values:liability_loss_description[0]) as liability_loss_description, to_varchar(field_values:other_loss_description[0]) as other_loss_description    
-from fivetran_covertree.dynamodb.prod_socotra_claims_table
+from fivetran_covertree.dynamodb.prod_socotra_claims_table where pk like 'POLICY#%'
