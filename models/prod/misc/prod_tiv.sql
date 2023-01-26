7f225612ec15b97595abd3401dccb2f5a6f28ccb
Trong Le <trong@covertree.com>
2023-01-26 13:33:20 -0600

    added coverage b, converted to doubles

--- models/prod/misc/prod_tiv.sql
+++ models/prod/misc/prod_tiv.sql
@@ -2,8 +2,9 @@
 
 select
     pk,
-  max(case when name = 'manufactured_home_limit' then value end) coverage_a,
-  max(case when name = 'unscheduled_personal_property_limit' then value end) coverage_c,
+  max(case when name = 'manufactured_home_limit' then to_double(value) end) coverage_a,
+  max(case when name = 'other_structures_limit' then to_double(value) end)
+  max(case when name = 'unscheduled_personal_property_limit' then to_double(value) end) coverage_c,
   max(case when name = 'loss_of_use_percentage' then to_double((left(value,2))/100) end) coverage_d
 from fivetran_covertree.transformations_dynamodb.prod_policy_perils_values_unique_view
 group by pk
