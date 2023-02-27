7f1d95b7b4c9f8c7f6fa9794362e861d31be3d8a
Trong Le <trong@covertree.com>
2023-02-27 10:28:56 -0600

    removing more test policies/quotes

--- models/prod/policies/prod_policies_policy.sql
+++ models/prod/policies/prod_policies_policy.sql
@@ -35,7 +35,8 @@ where (pk like 'POLICY#%' and sk like 'POLICY') and _fivetran_deleted='FALSE' an
   locator not in ('101255772', '100599038', '100640796', '100712180', '100825636', '100825766', '100825954',
   '100826086', '100849710', '100902420', '100902972', '100903084', '100931144', '100931232', '100957670',
   '100979166', '101178036', '101132926', '101298090', '101298206', '101298618', '101298854', '101299186',
-  '101255196', '101227402', '101290498', '101291026', '101291182', '101291550', '101291884', '101212396'))
+  '101255196', '101227402', '101290498', '101291026', '101291182', '101291550', '101291884', '101212396',
+  '101436546'))
 union
 (select deal_id, null, null, property_createdate, property_policy_effective_date, null, null, null, null, null, null, null, null, null, null, property_policy_end_date,
   null, null, null, null, null, null, property_policy_number, null, null, iff(property_policy_end_date > current_timestamp(), 'Policy-Activated', 'Policy-Cancelled'),
