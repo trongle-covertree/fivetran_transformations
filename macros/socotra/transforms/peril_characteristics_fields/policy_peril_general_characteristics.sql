e1cbe30495729d7b8985672c634c73988e3e028e
Trong Le <trong@covertree.com>
2023-03-09 14:50:10 -0600

    fixed peril groupings

--- macros/socotra/transforms/peril_characteristics_fields/policy_peril_general_characteristics.sql
+++ macros/socotra/transforms/peril_characteristics_fields/policy_peril_general_characteristics.sql
@@ -29,17 +29,19 @@ select
 	min(Case when field_name = 'occasional_vacation_rental' then field_value::boolean end) occasional_vacation_rental,
 	min(Case when field_name = 'residence_burglary_limit' then regexp_replace(field_value, '[$,]')::int End) residence_burglary_limit,
 	min(Case when field_name = 'equipment_breakdown_limit' then regexp_replace(field_value, '[$,]')::int end) equipment_breakdown_limit,
-    exposure_characteristics_locator,
+	exposure_locator,
 	pc.policy_locator::varchar as policy_locator,
-    to_timestamp_tz(pcf.datamart_created_timestamp/1000) as datamart_created_timestamp,
-    to_timestamp_tz(pcf.datamart_updated_timestamp/1000) as datamart_updated_timestamp
+	to_timestamp_tz(pcf.datamart_created_timestamp/1000) as datamart_created_timestamp,
+	to_timestamp_tz(pcf.datamart_updated_timestamp/1000) as datamart_updated_timestamp
 from {{ socotra_db }}.peril_characteristics_fields as pcf
 	inner join {{ socotra_db }}.peril_characteristics as pc
 		on pc.locator = pcf.peril_characteristics_locator
+	inner join {{ socotra_db }}.peril as p
+		on p.locator = pc.peril_locator
 where parent_name is null
 {% if is_incremental() %}
     and (to_timestamp_tz(pcf.datamart_created_timestamp/1000) > (select datamart_created_timestamp from {{ sf_schema }}.policy_peril_general_characteristics order by datamart_created_timestamp desc limit 1)
       or to_timestamp_tz(pcf.datamart_updated_timestamp/1000) > (select datamart_updated_timestamp from {{ sf_schema }}.policy_peril_general_characteristics order by datamart_updated_timestamp desc limit 1))
 {% endif %}
-group by pc.exposure_characteristics_locator, pcf.datamart_created_timestamp, pcf.datamart_updated_timestamp, pc.policy_locator
+group by exposure_locator, pcf.datamart_created_timestamp, pcf.datamart_updated_timestamp, pc.policy_locator
 {% endmacro %}
