e050f60dd6071ce327a32463dcee267076659b26
Trong Le <trong@covertree.com>
2023-03-27 09:54:45 -0500

    forgot to unit_address

--- macros/socotra/transforms/exposure_characteristics_fields/policy_exposure_unit_address.sql
+++ macros/socotra/transforms/exposure_characteristics_fields/policy_exposure_unit_address.sql
@@ -8,6 +8,7 @@ select
 	min(case when field_name = 'zip_code' then field_value end) as zip_code,
 	min(case when field_name = 'county' then field_value end) as county,
 	min(case when field_name = 'country' then field_value end) as country,
+	exposure_locator,
 	exposure_characteristics_locator,
     ec.policy_locator::varchar as policy_locator,
 	to_timestamp_tz(ecf.datamart_created_timestamp/1000) as datamart_created_timestamp,
@@ -20,5 +21,5 @@ from  {{ socotra_db }}.exposure_characteristics_fields as ecf
     and (to_timestamp_tz(ecf.datamart_created_timestamp/1000) > (select datamart_created_timestamp from {{ sf_schema }}.policy_exposure_unit_address order by datamart_created_timestamp desc limit 1)
       or to_timestamp_tz(ecf.datamart_updated_timestamp/1000) > (select datamart_updated_timestamp from {{ sf_schema }}.policy_exposure_unit_address order by datamart_updated_timestamp desc limit 1))
 {% endif %}
-group by ecf.exposure_characteristics_locator, ecf.datamart_created_timestamp, ecf.datamart_updated_timestamp, ec.policy_locator
+group by exposure_locator, ecf.exposure_characteristics_locator, ecf.datamart_created_timestamp, ecf.datamart_updated_timestamp, ec.policy_locator
 {% endmacro %}
