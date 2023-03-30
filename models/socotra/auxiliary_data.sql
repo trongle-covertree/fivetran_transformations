aef9e56e5fd8271ae429ebe32cb4a1be27bcb645
Trong Le <trong@covertree.com>
2023-03-30 09:19:51 -0500

    added unique_keys for auxiliary_data

--- models/socotra/auxiliary_data.sql
+++ models/socotra/auxiliary_data.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental', unique_key=['locator', 'key_name']) }}
 
 {{ run_socotra_policy_auxiliary_data('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
