9caeec2599eb8369588c9f2dc0e4f959dfca8152
Trong Le <trong@covertree.com>
2023-04-05 16:27:12 -0500

    added unique_keys for exposure characteristic fields

--- models/socotra/transforms/exposure_characteristics_fields/policy_exposure_unit_details.sql
+++ models/socotra/transforms/exposure_characteristics_fields/policy_exposure_unit_details.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental', unique_key=['exposure_characteristics_locator','policy_locator']) }}
 
 {{ run_socotra_exposure_unit_details('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
