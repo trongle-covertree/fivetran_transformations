7ed7254156a7dec3c531c67423a8567d942939c1
Trong Le <trong@covertree.com>
2023-04-05 17:13:00 -0500

    trying quote_exposure_unit_address without timestamps

--- macros/socotra/transforms/exposure_characteristics_fields/quote_exposure_unit_address.sql
+++ macros/socotra/transforms/exposure_characteristics_fields/quote_exposure_unit_address.sql
@@ -11,16 +11,10 @@ select
 	quote_exposure_locator,
 	quote_exposure_characteristics_locator,
 	quote_policy_locator,
-    ec.policy_locator::varchar as policy_locator,
-	to_timestamp_tz(ecf.datamart_created_timestamp/1000) as datamart_created_timestamp,
-	to_timestamp_tz(ecf.datamart_updated_timestamp/1000) as datamart_updated_timestamp
+    ec.policy_locator::varchar as policy_locator
 from  {{ socotra_db }}.quote_exposure_characteristics_fields as ecf
 	inner join {{ socotra_db }}.quote_exposure_characteristics as ec
 		on ec.locator = ecf.quote_exposure_characteristics_locator
 	where parent_name = 'unit_address'
-{% if is_incremental() %}
-    and (to_timestamp_tz(ecf.datamart_created_timestamp/1000) > (select datamart_created_timestamp from {{ sf_schema }}.quote_exposure_unit_address order by datamart_created_timestamp desc limit 1)
-      or to_timestamp_tz(ecf.datamart_updated_timestamp/1000) > (select datamart_updated_timestamp from {{ sf_schema }}.quote_exposure_unit_address order by datamart_updated_timestamp desc limit 1))
-{% endif %}
-group by quote_exposure_locator, ecf.quote_exposure_characteristics_locator, ecf.datamart_created_timestamp, ecf.datamart_updated_timestamp, ec.quote_policy_locator, ec.policy_locator
+group by quote_exposure_locator, ecf.quote_exposure_characteristics_locator, ec.quote_policy_locator, ec.policy_locator
 {% endmacro %}
