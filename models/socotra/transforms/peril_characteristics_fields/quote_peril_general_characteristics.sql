2e3167138cdab8ec6a41e6f04796052a076f5045
Trong Le <trong@covertree.com>
2023-04-11 16:41:00 -0500

    removing unique key for quote_peril_general_characteristic

--- models/socotra/transforms/peril_characteristics_fields/quote_peril_general_characteristics.sql
+++ models/socotra/transforms/peril_characteristics_fields/quote_peril_general_characteristics.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental', unique_key=['quote_exposure_locator','quote_policy_locator']) }}
+{{ config(materialized='incremental' )}}
 
 {{ run_socotra_quote_peril_general_chars('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
