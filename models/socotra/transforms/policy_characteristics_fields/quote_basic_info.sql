fc0ef90e9a13580acdfde756f820ca4d7f73936c
Trong Le <trong@covertree.com>
2023-04-14 13:00:10 -0500

    removing unique keys for quote policy level characteristics

--- models/socotra/transforms/policy_characteristics_fields/quote_basic_info.sql
+++ models/socotra/transforms/policy_characteristics_fields/quote_basic_info.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental', unique_key=['quote_policy_characteristics_locator', 'quote_policy_locator']) }}
+{{ config(materialized='incremental') }}
 
 {{ run_socotra_quote_basic_info('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
--- models/socotra/transforms/policy_characteristics_fields/quote_level_suppl_uw.sql
+++ models/socotra/transforms/policy_characteristics_fields/quote_level_suppl_uw.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental', unique_key=['quote_policy_characteristics_locator', 'quote_policy_locator']) }}
+{{ config(materialized='incremental') }}
 
 {{ run_socotra_quote_level_suppl_uw('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
