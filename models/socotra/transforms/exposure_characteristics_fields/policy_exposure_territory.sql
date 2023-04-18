be419ae06962ecc283f7064316f135fbf34bc97d
Trong Le <trong@covertree.com>
2023-04-18 11:22:03 -0500

    updated incremental_strategy to delete+insert for policies

--- models/socotra/transforms/exposure_characteristics_fields/policy_exposure_territory.sql
+++ models/socotra/transforms/exposure_characteristics_fields/policy_exposure_territory.sql
@@ -1,3 +1,5 @@
-{{ config(materialized='incremental', unique_key=['exposure_characteristics_locator','policy_locator']) }}
+{{ config(materialized='incremental',
+    unique_key=['exposure_characteristics_locator','policy_locator'],
+    incremental_strategy='delete+insert') }}
 
 {{ run_socotra_exposure_territory('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
--- models/socotra/transforms/policy_characteristics_fields/policy_agency_information.sql
+++ models/socotra/transforms/policy_characteristics_fields/policy_agency_information.sql
@@ -1,3 +1,5 @@
-{{ config(materialized='incremental', unique_key=['policy_characteristics_locator', 'policy_locator']) }}
+{{ config(materialized='incremental',
+    unique_key=['policy_characteristics_locator', 'policy_locator'],
+    incremental_strategy='delete+insert') }}
 
 {{ run_socotra_policy_agency_information('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
