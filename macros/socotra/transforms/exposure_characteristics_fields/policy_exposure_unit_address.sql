0eabec6d6e81cc25e7ff3366d571d8f154e865bb
Trong Le <trong@covertree.com>
2023-04-25 12:29:29 -0500

    putting modifications back

--- macros/socotra/transforms/exposure_characteristics_fields/policy_exposure_unit_address.sql
+++ macros/socotra/transforms/exposure_characteristics_fields/policy_exposure_unit_address.sql
@@ -11,6 +11,7 @@ select
 	exposure_locator,
 	ecf.exposure_characteristics_locator,
     ec.policy_locator::varchar as policy_locator,
+	policy_modification_locator,
 	to_timestamp_tz(ec.start_timestamp/1000) as start_timestamp,
     to_timestamp_tz(ec.end_timestamp/1000) as end_timestamp,
 	to_timestamp_tz(ec.datamart_created_timestamp/1000) as datamart_created_timestamp,
@@ -18,10 +19,12 @@ select
 from  {{ socotra_db }}.exposure_characteristics_fields as ecf
 	inner join {{ socotra_db }}.exposure_characteristics as ec
 		on ec.locator = ecf.exposure_characteristics_locator
+	inner join {{ socotra_db }}.peril_characteristics as pc
+		on ec.locator = pc.exposure_characteristics_locator
 	where parent_name = 'unit_address'
 {% if is_incremental() %}
     and (to_timestamp_tz(ec.datamart_created_timestamp/1000) > (select datamart_created_timestamp from {{ sf_schema }}.policy_exposure_unit_address order by datamart_created_timestamp desc limit 1)
       or to_timestamp_tz(ec.datamart_updated_timestamp/1000) > (select datamart_updated_timestamp from {{ sf_schema }}.policy_exposure_unit_address order by datamart_updated_timestamp desc limit 1))
 {% endif %}
-group by exposure_locator, ecf.exposure_characteristics_locator, ec.datamart_created_timestamp, ec.datamart_updated_timestamp, ec.policy_locator, ec.start_timestamp, ec.end_timestamp
+group by exposure_locator, ecf.exposure_characteristics_locator, ec.datamart_created_timestamp, ec.datamart_updated_timestamp, ec.policy_locator, policy_modification_locator, ec.start_timestamp, ec.end_timestamp
 {% endmacro %}
