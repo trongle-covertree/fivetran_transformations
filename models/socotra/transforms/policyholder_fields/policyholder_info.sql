a438aef4eb74c3c44eb10477ae139739d46fcaf4
Trong Le <trong@covertree.com>
2023-05-18 17:32:42 -0500

    removed delete+insert in policyholder transforms

--- models/socotra/transforms/policyholder_fields/policyholder_info.sql
+++ models/socotra/transforms/policyholder_fields/policyholder_info.sql
@@ -1,5 +1,4 @@
 {{ config(materialized='incremental',
-    unique_key=['policyholder_locator'],
-    incremental_strategy='delete+insert') }}
+    unique_key=['policyholder_locator']) }}
 
 {{ run_socotra_policyholder_info('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
