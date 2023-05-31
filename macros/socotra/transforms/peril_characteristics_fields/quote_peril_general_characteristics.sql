1c95d6fbcaa6fc07cebc48811021f91910a349be
Trong Le <trong@covertree.com>
2023-05-31 17:23:20 -0500

    switching quote_peril_gen table to not incremental

--- macros/socotra/transforms/peril_characteristics_fields/quote_peril_general_characteristics.sql
+++ macros/socotra/transforms/peril_characteristics_fields/quote_peril_general_characteristics.sql
@@ -40,18 +40,12 @@ select
 	max(Case when field_name = 'mine_subsidence_deductible_ohio' then regexp_replace(field_value, '[$,]') end) mine_subsidence_deductible_ohio,
 	quote_exposure_locator,
 	pc.quote_policy_locator,
-	pc.policy_locator::varchar as policy_locator,
-	to_timestamp_tz(pc.datamart_created_timestamp/1000) as datamart_created_timestamp,
-	to_timestamp_tz(pc.datamart_updated_timestamp/1000) as datamart_updated_timestamp
+	pc.policy_locator::varchar as policy_locator
 from {{ socotra_db }}.quote_peril_characteristics_fields as pcf
 	inner join {{ socotra_db }}.quote_peril_characteristics as pc
 		on pc.locator = pcf.quote_peril_characteristics_locator
 	inner join {{ socotra_db }}.quote_peril as p
 		on p.locator = pc.quote_peril_locator
 where parent_name is null and p.name not in ('Scheduled Personal Property', 'Policy Minimum Premium Coverage', 'Identity Fraud Expense')
-{% if is_incremental() %}
-    and (to_timestamp_tz(pc.datamart_created_timestamp/1000) > (select datamart_created_timestamp from {{ sf_schema }}.quote_peril_general_characteristics order by datamart_created_timestamp desc limit 1)
-      or to_timestamp_tz(pc.datamart_updated_timestamp/1000) > (select datamart_updated_timestamp from {{ sf_schema }}.quote_peril_general_characteristics order by datamart_updated_timestamp desc limit 1))
-{% endif %}
-group by quote_exposure_locator, pc.datamart_created_timestamp, pc.datamart_updated_timestamp, pc.quote_policy_locator, pc.policy_locator
+group by quote_exposure_locator, pc.quote_policy_locator, pc.policy_locator
 {% endmacro %}
--- models/socotra/transforms/peril_characteristics_fields/quote_peril_general_characteristics.sql
+++ models/socotra/transforms/peril_characteristics_fields/quote_peril_general_characteristics.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental', unique_key=['quote_exposure_locator', 'quote_policy_locator'], incremental_strategy='delete+insert') }}
+{{ config(materialized='table') }}
 
 {{ run_socotra_quote_peril_general_chars('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
