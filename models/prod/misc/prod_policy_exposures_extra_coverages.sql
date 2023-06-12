d9c3e84075fe314a8e1e46a0f7e3fd461464a6c4
Trong Le <trong@covertree.com>
2023-06-12 15:06:58 -0500

    removed extra_coverages and lender_info transformations

--- models/prod/misc/prod_policy_exposures_extra_coverages.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='incremental') }}
-
-{{ run_exposures_extra_coverages( env='transformations_dynamodb', prefix='prod' )}}
--- models/prod/misc/prod_policy_exposures_lender_info.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='incremental') }}
-
-{{ run_exposures_lender_info( env='transformations_dynamodb', prefix='prod' )}}
