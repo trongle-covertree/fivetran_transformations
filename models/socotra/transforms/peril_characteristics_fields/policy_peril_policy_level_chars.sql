13a4a3f1e1e1c9dec34e1cbf55a0f3d3ee1f6ebf
Trong Le <trong@covertree.com>
2023-06-15 14:28:47 -0500

    adding in delete+insert for policy level chars

--- models/socotra/transforms/peril_characteristics_fields/policy_peril_policy_level_chars.sql
+++ models/socotra/transforms/peril_characteristics_fields/policy_peril_policy_level_chars.sql
@@ -1,3 +1,3 @@
-{{ config(materialized='incremental', unique_key=['exposure_locator', 'policy_locator']) }}
+{{ config(materialized='incremental', unique_key=['exposure_locator', 'policy_locator'], incremental_strategy='delete+insert') }}
 
 {{ run_socotra_peril_policy_level_chars('mysql_data_mart_10001', 'transformations_prod_socotra')  }}
