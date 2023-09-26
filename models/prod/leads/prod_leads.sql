53e9bd2cdb58a53b8d3e8a7216d1b7c0b1bbdef2
Trong Le <trong@covertree.com>
2023-09-26 10:17:58 -0500

    switched leads to new schema

--- models/prod/leads/prod_leads.sql
+++ models/prod/leads/prod_leads.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='incremental', unique_key='PK') }}
+{{ config(materialized='table') }}
 
 select
   PK,
@@ -6,10 +6,9 @@ select
   CREATED_AT,
   to_timestamp_tz(to_varchar(UPDATED_AT)) as UPDATED_AT,
   POLICY_LOCATOR,
-  STATUS
+  STATUS,
+  PARTNER_KEY,
+  CREDIT,
+  TREMENDOUS_ORDER_ID
 from dynamodb.prod_leads_table
 where _fivetran_deleted='FALSE'
-{% if is_incremental() %}
-  and (CREATED_AT > (select CREATED_AT from transformations_dynamodb.prod_leads order by CREATED_AT desc limit 1)
-  or to_timestamp_tz(to_varchar(UPDATED_AT)) > (select UPDATED_AT from transformations_dynamodb.prod_leads order by UPDATED_AT desc limit 1))
-{% endif %}
